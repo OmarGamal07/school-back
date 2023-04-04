d7dbfa74313458e42041312c8e3a7b2a9cbee633
Mohammed <mohammedadell301@gmail.com>
2023-04-04 02:36:05 +0200

    admin and teacher middleware and courseProgram CRUD Operation routes

---
+++ models/admin/courseProgram.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  video: { type: String },
+});
+
+module.exports = mongoose.model("Course", courseSchema);
