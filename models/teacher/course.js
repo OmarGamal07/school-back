9d4d6df17c01df5cc741750c55a9997e0283c30d
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-04-04 02:43:19 +0200

    adding route course

--- models/teacher/course.js
+++ models/teacher/course.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  date: { type: Date, required: true },
+  Date: { type: Date, required: true },
   courseProgram: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CourseProgram' }],
-  notes: [{ studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' }, note: { type: String } }],
+  notes: [{ studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'note' }, note: { type: String } }],
 });
 
 module.exports = mongoose.model('Course', courseSchema);
